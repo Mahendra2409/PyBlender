11369a61001b67bb6bb6d33d83aa6331c9acb163
Mahendra2409 <ms5268275@gmail.com>
2026-05-12 10:47:01 +0530

    feat: update armadillo PLY rendering configuration and enhance rendering logic

---
+++ Deprecate/Colored_PLY/RueMadame_Render.py
@@ -0,0 +1,27 @@
+import os
+import RueMadame as Render_Engine
+
+# Get the current working directory.
+# It's assumed that this script is run from a directory
+# which contains the 'Data\data.ply(Colored_PLY)' subdirectory structure.
+project_root_cwd = os.getcwd()
+print(f"Current working directory: {project_root_cwd}")
+
+input_folder = os.path.join(project_root_cwd, "Data", "data.ply(Colored_PLY)", "kinect", "kinect", "RueMadame_PLY")
+output_folder = os.path.join(project_root_cwd, "Data", "data.ply(Colored_PLY)", "Output", "RueMadame")
+
+os.makedirs(output_folder, exist_ok=True)
+
+# Loop through files
+for filename in os.listdir(input_folder):
+    # if filename.endswith(".ply"):
+    
+    meshPath = os.path.join(input_folder, filename)
+    outputPath = os.path.join(output_folder, filename.replace(".ply", ".png"))
+    if os.path.exists(outputPath):
+        print(f"Output file already exists: {outputPath}. Skipping...")
+        continue
+    
+    if filename.endswith(".ply"):
+        print(f"Rendering [{filename}]...")
+        Render_Engine.render(meshPath, outputPath)
